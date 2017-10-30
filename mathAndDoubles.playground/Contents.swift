ee1f15253fae0ad4b1f911fda80c1813e760ac20
West Kraemer <kraemerwest@gmail.com>
2017-10-30 14:44:19 -0400

    initial commit

---
+++ mathAndDoubles.playground/Contents.swift
@@ -0,0 +1,71 @@
+//Math and Doubles
+
+13 - 16
+
+100 / 25
+
+var money = 200
+
+var payday = 500
+
+money = (payday * 12) + money
+
+print(money)
+
+var favCandy : [String] = ["100 Grand Bar", "Snickers", "Mars", "Fun Dip", "Sour Patch Kids"]
+
+print(favCandy)
+
+favCandy.append("Hershey")
+
+print(favCandy)
+
+var luckyNums = [34, 52, 353, 43]
+
+print(luckyNums)
+
+luckyNums.append(Int("54")!)
+
+print(luckyNums)
+
+favCandy.insert(String(payday), at: 1)
+
+print(favCandy)
+
+favCandy[3] = "Almond Joy"
+
+print(favCandy)
+
+favCandy.insert("Mars", at: 3)
+
+print(favCandy)
+
+var numbers = [1,2,4,5]
+
+numbers.insert(3, at: 2)
+
+print(numbers)
+
+numbers[0] = 88
+
+print(numbers)
+
+numbers.remove(at: 3)
+
+print(numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
