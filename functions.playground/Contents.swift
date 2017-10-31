1b08be48a4492bcd4797dee289a0bf17d78baa0a
West Kraemer <kraemerwest@gmail.com>
2017-10-31 12:17:22 -0400

    add return parameters to functions

--- functions.playground/Contents.swift
+++ functions.playground/Contents.swift
@@ -11,22 +11,29 @@ hello()
 hello()
 
 
-func printHamster(theString: String) {
-    print(theString + "🐹")
+
+func printHamster(theString: String) -> String {
+    return theString + "🐹"
 }
-printHamster(theString: "hamster emoji = ")
+
+printHamster(theString: "hamster")
 
 
 
-func addNums(number1:Int, number2:Int) {
-    print(number1 + number2)
+//printHamster(theString: "hamster emoji = ")
+
+
+func addNums(number1:Int, number2:Int) -> Int {
+    return number1 + number2
 }
 
 addNums(number1: 5, number2: 6)
 
-func multNums(num1: Int, num2: Int) {
-    print(num1 * num2)
+func multNums(num1: Double, num2: Double) -> Double {
+    return num1 * num2
 }
 
-multNums(num1: 4, num2: 10)
+multNums(num1: 4.34563, num2: 10.253499999999999999)
 
+
+addNums(number1: 2, number2: 7)
