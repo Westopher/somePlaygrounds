f5405027a6bfdb0d7d1e25b84decbd4c2da50f04
West Kraemer <kraemerwest@gmail.com>
2017-11-15 05:33:06 -0500

    demonstrate different ways of working with optionals

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,11 +1,15 @@
 
 var favCandy : String? = nil
 
-favCandy = "Snickers"
+//favCandy = "Snickers"
 
-if favCandy == nil {
-   print("1")
-} else {
-    print("2")
-    favCandy!
+//if favCandy == nil {
+//   print("1")
+//} else {
+//    print("2")
+//    favCandy!
+//}
+
+if let favCandyUnwrapped = favCandy {
+    print(favCandyUnwrapped)
 }
