ccfc9543567b9d02094d522c59bf132bd1d77e97
West Kraemer <kraemerwest@gmail.com>
2017-11-15 07:55:33 -0500

    add unwrapping optionals and working with subclasses

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -13,3 +13,28 @@ var favCandy : String? = nil
 if let favCandyUnwrapped = favCandy {
     print(favCandyUnwrapped)
 }
+
+//working with classes and subclasses
+
+class Animal {
+    var name = ""
+    var age = 0
+}
+
+class Dog : Animal {
+    var furColor = "brown"
+}
+
+class Monkey : Animal {
+    var tail = ""
+}
+
+var someAnimal : Animal = Dog()
+
+//if this variable (luna) can be assigned to this other variable as a dog object, then unwrap it. It'll do so if it can with no error.
+if let Luna = someAnimal as? Dog {
+    print(Luna.furColor)
+}
+
+
+
